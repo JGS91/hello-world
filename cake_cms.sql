639a188dc86b34c621df9e8163a39a6af83b382b
JGS91 <36439323+JGS91@users.noreply.github.com>
2018-02-13 16:43:17 +0000

    Add files via upload

--- cake_cms.sql
+++ cake_cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 13, 2018 at 06:54 AM
+-- Generation Time: Feb 13, 2018 at 04:41 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.9
 
@@ -48,23 +48,9 @@ CREATE TABLE IF NOT EXISTS `articles` (
 --
 
 INSERT INTO `articles` (`id`, `user_id`, `title`, `slug`, `body`, `created`, `modified`, `published`) VALUES
-(3, 1, 'Article Two', 'Article-Two', 'Article Two Body', '2018-02-09 19:16:09', '2018-02-09 19:16:09', '2018-02-15'),
+(3, 1, 'Article Two', 'Article-Two', 'Article Two Body', '2018-02-09 19:16:09', '2018-02-13 16:33:01', '2018-02-15'),
 (4, 1, 'Article three', 'Article-three', 'Article three body', '2018-02-09 20:42:59', '2018-02-09 20:42:59', NULL),
-(5, 2, 'test date test date', 'test-date-test-date', 'testing the date', '2018-02-12 22:15:23', '2018-02-12 22:15:23', '2018-02-15'),
-(6, 2, 'asdasdasdasdasd', 'asdasdasdasdasd', 'asdddddddddd', '2018-02-12 22:15:48', '2018-02-12 22:15:48', '2018-02-23');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `articles_comments`
---
-
-DROP TABLE IF EXISTS `articles_comments`;
-CREATE TABLE IF NOT EXISTS `articles_comments` (
-  `article_id` int(11) NOT NULL,
-  `comment_id` int(11) NOT NULL,
-  `comment_body` varchar(300) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+(5, 2, 'test date test date', 'test-date-test-date', 'testing the date', '2018-02-12 22:15:23', '2018-02-12 22:15:23', '2018-02-15');
 
 -- --------------------------------------------------------
 
@@ -86,6 +72,7 @@ CREATE TABLE IF NOT EXISTS `articles_tags` (
 
 INSERT INTO `articles_tags` (`article_id`, `tag_id`) VALUES
 (3, 2),
+(3, 3),
 (4, 3);
 
 -- --------------------------------------------------------
@@ -97,11 +84,23 @@ INSERT INTO `articles_tags` (`article_id`, `tag_id`) VALUES
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `articles_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `text` text NOT NULL,
-  `created` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `article_key` (`article_id`),
+  KEY `user_key` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `article_id`, `user_id`, `text`, `created`) VALUES
+(1, 3, 2, 'Hi this is a test. A test of a comment for atricle Two by the user jgs23.', '2018-02-13 03:00:00'),
+(2, 4, 1, 'Hi this is a test this is not for article 2 but one of the others.', '2018-02-24 00:00:00'),
+(3, 3, 2, 'Hi test for article 2 again', '2018-02-27 00:00:00');
 
 -- --------------------------------------------------------
 
