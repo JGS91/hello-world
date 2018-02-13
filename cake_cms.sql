2a3977f8aff8ba60c3dbf3f54a63a10060dad5d3
JGS91 <36439323+JGS91@users.noreply.github.com>
2018-02-13 09:30:05 +0000

    Add files via upload

---
+++ cake_cms.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 13, 2018 at 06:54 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cake_cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(191) NOT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `published` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `user_key` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `user_id`, `title`, `slug`, `body`, `created`, `modified`, `published`) VALUES
+(3, 1, 'Article Two', 'Article-Two', 'Article Two Body', '2018-02-09 19:16:09', '2018-02-09 19:16:09', '2018-02-15'),
+(4, 1, 'Article three', 'Article-three', 'Article three body', '2018-02-09 20:42:59', '2018-02-09 20:42:59', NULL),
+(5, 2, 'test date test date', 'test-date-test-date', 'testing the date', '2018-02-12 22:15:23', '2018-02-12 22:15:23', '2018-02-15'),
+(6, 2, 'asdasdasdasdasd', 'asdasdasdasdasd', 'asdddddddddd', '2018-02-12 22:15:48', '2018-02-12 22:15:48', '2018-02-23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles_comments`
+--
+
+DROP TABLE IF EXISTS `articles_comments`;
+CREATE TABLE IF NOT EXISTS `articles_comments` (
+  `article_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `comment_body` varchar(300) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles_tags`
+--
+
+DROP TABLE IF EXISTS `articles_tags`;
+CREATE TABLE IF NOT EXISTS `articles_tags` (
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`article_id`,`tag_id`),
+  KEY `tag_key` (`tag_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `articles_tags`
+--
+
+INSERT INTO `articles_tags` (`article_id`, `tag_id`) VALUES
+(3, 2),
+(4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `articles_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `title`, `created`, `modified`) VALUES
+(1, 'Tag', '2018-02-09 19:11:13', '2018-02-09 19:11:13'),
+(2, 'Tag 2', '2018-02-09 19:11:22', '2018-02-09 19:11:22'),
+(3, 'tag3', '2018-02-09 20:42:59', '2018-02-09 20:42:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(6) NOT NULL DEFAULT 'reader',
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role`, `email`, `password`, `created`, `modified`) VALUES
+(1, 'reader', 'cakephp@example.com', '$2y$10$WaQ52aMcWMEd8u8TH1ZPfOxKr5sN5VTAdRLQ43vNADnlep3w7i5WO', '2018-02-07 22:31:36', '2018-02-09 21:09:26'),
+(2, 'author', 'jgs23101991@gmail.com', '$2y$10$.uVxcRUWaq4nSppys6I/hO97TWU77wLzaOi4Z1TwVrJM3wpi4jBq.', '2018-02-11 16:00:44', '2018-02-11 16:00:44');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
